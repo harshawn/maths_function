5193ffa366d06787f2f6c60a7eddc10d979eb8d7
Harshawn Grewal <P13232291@GH582-002.lec-admin.dmu.ac.uk>
2015-02-05 11:09:33 +0000

    Added functions program

---
+++ functions.c
@@ -0,0 +1,27 @@
+//Functionality to add, subtract, multiply,divide
+//with each option added in seprate commit
+
+#include <stdio.h>
+
+int main() 
+{
+	int first, second, add, subtract, multiply;
+	float divide;
+	
+	printf("Enter first integer: ");
+	scanf("%d", &first);
+	printf("Enter second integer: ");
+	scanf("%d", &second);
+	
+	add = first + second;
+//	subtract = first - second;
+//	multiply = first * second;
+//	divide = first / second;
+	
+	printf("Addition = %d\n", add);
+//	printf("Subtraction = %d\n", &subtract);
+//	printf("Multiply = %d\n", &multiply);
+//	printf("Divide = %d\n", &divide);
+
+	return 0;
+}
